16f57099d12d6740acae2a24f9cf069838db8a9d
colin <colin@ba3075da-5205-0410-b03c-ad9aa4846add>
2009-05-21 21:09:52 +0000

    Remove extra WebGUI directory created by release process.
    git-svn-id: https://svn.webgui.org/plainblack/releases/WebGUI_7.7.7-beta@10948 ba3075da-5205-0410-b03c-ad9aa4846add

--- WebGUI/www/extras/textFix.js
+++
@@ -1,4 +0,0 @@
-function fixChars(element) { 
-	element.value = element.value.replace(/-/mg,"-"); 
-}
-
--- WebGUI/www/extras/tinymce-webgui/plugins/wginsertimage/langs/en.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('en.wginsertimage',{
-    desc        : 'Insert WebGUI Image'
-});
